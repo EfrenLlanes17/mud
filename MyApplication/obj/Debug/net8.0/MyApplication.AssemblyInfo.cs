d6b8af16d6a396341f49a2fc9c1d73708cb0770f
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-03-14 13:50:06 -0500

    Halfway through 10 still

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99013ce7ce31c225dc6ade3f8ae82ac0c9fb1a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d9e0524aa260e9839853b1d8c000e763a9aec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
